92d7bbe16b4732f84aa4187a16df3e8af19f81e6
Hugh Coleman <46972988+hjc2@users.noreply.github.com>
2024-10-24 11:28:23 -0500

    removed unnecessary imports

--- Assets/Scenes/GameManager.cs
+++ Assets/Scenes/GameManager.cs
@@ -1,13 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-
-using UnityEngine.Tilemaps;
-using UnityEngine;
 using UnityEngine.Tilemaps;
 using System;
-using System.Collections.Generic;
 
 
 [Serializable]
