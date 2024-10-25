8e9b5e208299f42e724b57f84c1b11af45250ef7
Hugh Coleman <46972988+hjc2@users.noreply.github.com>
2024-10-25 15:05:48 -0500

    new scene example

--- Assets/guard/GuardController.cs
+++ Assets/guard/GuardController.cs
@@ -65,7 +65,7 @@ public class Guard : MonoBehaviour
         if (direction != Vector3.zero)
         {
             float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-            Quaternion targetRotation = Quaternion.Euler(0, 0, angle - 90);
+            Quaternion targetRotation = Quaternion.Euler(0, 0, angle + 90);
             transform.rotation = Quaternion.RotateTowards(transform.rotation,
                                                         targetRotation,
                                                         rotationSpeed * Time.deltaTime);
