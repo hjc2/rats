745b0582835089cb7a3dd2ac469a72e986211627
Hugh Coleman <46972988+hjc2@users.noreply.github.com>
2024-11-19 10:57:41 -0600

    switch reset code removal

--- Assets/switch/switch.cs
+++ Assets/switch/switch.cs
@@ -13,92 +13,6 @@ public class SwitchController : MonoBehaviour
     private Color brightYellow = new Color(1f, 1f, 0f);      // Bright yellow
     private Color darkYellow = new Color(0.1f, 0.2f, 0.1f);    // Dark yellow
 
-    // Dictionary to store original states of components
-    private Dictionary<Component, bool> originalStates = new Dictionary<Component, bool>();
-    private Dictionary<Renderer, Color> originalColors = new Dictionary<Renderer, Color>();
-
-    private void Start()
-    {
-        StoreOriginalStates();
-    }
-
-    private void StoreOriginalStates()
-    {
-        originalStates.Clear();
-        originalColors.Clear();
-
-        foreach (GameObject light in controlledLights)
-        {
-            if (light == null) continue;
-
-            // Store states of parent components
-            Component[] parentComponents = light.GetComponents<Component>();
-            foreach (Component component in parentComponents)
-            {
-                if (component is Behaviour behaviour)
-                {
-                    originalStates[component] = behaviour.enabled;
-                }
-                else if (component is Renderer renderer)
-                {
-                    originalStates[component] = renderer.enabled;
-                }
-                else if (component is Collider collider)
-                {
-                    originalStates[component] = collider.enabled;
-                }
-            }
-
-            // Store states and colors of child renderers
-            Renderer[] childRenderers = light.GetComponentsInChildren<Renderer>(true);
-            foreach (Renderer renderer in childRenderers)
-            {
-                if (renderer.gameObject != light)
-                {
-                    if (renderer is SpriteRenderer spriteRenderer)
-                    {
-                        originalColors[renderer] = spriteRenderer.color;
-                    }
-                }
-            }
-        }
-    }
-
-    public void ResetToOriginal()
-    {
-        foreach (var kvp in originalStates)
-        {
-            Component component = kvp.Key;
-            bool originalState = kvp.Value;
-
-            if (component is Behaviour behaviour)
-            {
-                behaviour.enabled = originalState;
-            }
-            else if (component is Renderer renderer)
-            {
-                renderer.enabled = originalState;
-            }
-            else if (component is Collider collider)
-            {
-                collider.enabled = originalState;
-            }
-        }
-
-        foreach (var kvp in originalColors)
-        {
-            Renderer renderer = kvp.Key;
-            Color originalColor = kvp.Value;
-
-            if (renderer is SpriteRenderer spriteRenderer)
-            {
-                spriteRenderer.color = originalColor;
-            }
-        }
-
-        // Reset the activation state to match the original state
-        isActivated = true;
-    }
 
     public void ToggleState()
     {
@@ -151,7 +65,6 @@ public class SwitchController : MonoBehaviour
 
     private void UpdateChildColors(GameObject parent, bool lightStatus)
     {
-        // Get all renderers in children
         Renderer[] childRenderers = parent.GetComponentsInChildren<Renderer>(true);
         foreach (Renderer renderer in childRenderers)
         {
